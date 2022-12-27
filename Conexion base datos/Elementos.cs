4fc854d898a2f287d206fa13df3bd5a8cd007b0f
OctavioMolina <octaviomolina2004@gmail.com>
2022-12-27 16:09:19 -0300

    primera subida...

---
+++ Conexion base datos/Elementos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conexion_base_datos
+{
+    internal class Elementos
+    {
+        public string medida { get; set; }
+        public string color { get; set; }
+        public int precio { get; set; }
+    }
+}
---
+++ Conexion base datos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Conexion_base_datos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                List<Elementos> listar()
+            {
+                List<Elementos> lista = new List<Elementos>();
+                SqlConnection conexion = new SqlConnection();
+                SqlCommand comando = new SqlCommand();
+                SqlDataReader lector;
+
+                try
+                {
+                    conexion.ConnectionString = "server=.\\SQLEXPRESS; database=(DB sobre la cual quiero trabajar); integrated security=true";
+                    comando.CommandType = System.Data.CommandType.Text;
+                    comando.CommandText = "(query a realizar)";
+                    comando.Connection = conexion;
+
+                    conexion.Open();
+                    lector = comando.ExecuteReader();
+
+                    while (lector.Read())
+                    {
+                        Elementos aux = new Elementos();
+                        aux.precio = lector.GetInt32(0);
+                        aux.color = (string)lector["color"];
+                        aux.medida = (string)lector["medida"];
+
+                        lista.Add(aux);
+                    }
+
+                    conexion.Close();
+                    return lista;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                listar();
+            }
+
+        }
+    }
+}
